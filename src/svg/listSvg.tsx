0dca4399615316ca393376654cb102e20dcb1ebc
John Doe <lucborn@gmail.com>
2024-09-17 17:45:08 +0300

    add svgs

--- src/svg/listSvg.tsx
+++ src/svg/listSvg.tsx
@@ -1,10 +1,10 @@
 export const CALENDAR = (<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_3_2958)">
-<path d="M12 3.33334H3.99999C3.26361 3.33334 2.66666 3.9303 2.66666 4.66668V12.6667C2.66666 13.4031 3.26361 14 3.99999 14H12C12.7364 14 13.3333 13.4031 13.3333 12.6667V4.66668C13.3333 3.9303 12.7364 3.33334 12 3.33334Z" stroke="#5ACEE8" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M10.6667 2V4.66667" stroke="#5ACEE8" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M5.33334 2V4.66667" stroke="#5ACEE8" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M2.66666 7.33334H13.3333" stroke="#5ACEE8" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M6.66668 10H5.33334V11.3333H6.66668V10Z" stroke="#5ACEE8" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+<g clipPath="url(#clip0_3_2958)">
+<path d="M12 3.33334H3.99999C3.26361 3.33334 2.66666 3.9303 2.66666 4.66668V12.6667C2.66666 13.4031 3.26361 14 3.99999 14H12C12.7364 14 13.3333 13.4031 13.3333 12.6667V4.66668C13.3333 3.9303 12.7364 3.33334 12 3.33334Z" stroke="#5ACEE8" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M10.6667 2V4.66667" stroke="#5ACEE8" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M5.33334 2V4.66667" stroke="#5ACEE8" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M2.66666 7.33334H13.3333" stroke="#5ACEE8" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M6.66668 10H5.33334V11.3333H6.66668V10Z" stroke="#5ACEE8" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
 </g>
 <defs>
 <clipPath id="clip0_3_2958">
@@ -15,9 +15,9 @@ export const CALENDAR = (<svg width="16" height="16" viewBox="0 0 16 16" fill="n
 
 export const BELL = (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_475_45)">
-<path d="M10 5C10 4.46957 10.2107 3.96086 10.5858 3.58579C10.9609 3.21071 11.4696 3 12 3C12.5304 3 13.0391 3.21071 13.4142 3.58579C13.7893 3.96086 14 4.46957 14 5C15.1484 5.54303 16.1274 6.38833 16.8321 7.4453C17.5367 8.50227 17.9404 9.73107 18 11V14C18.0753 14.6217 18.2954 15.2171 18.6428 15.7381C18.9902 16.2592 19.4551 16.6914 20 17H4C4.54494 16.6914 5.00981 16.2592 5.35719 15.7381C5.70457 15.2171 5.92474 14.6217 6 14V11C6.05956 9.73107 6.4633 8.50227 7.16795 7.4453C7.8726 6.38833 8.85159 5.54303 10 5" stroke="#616B77" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9 17V18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18V17" stroke="#616B77" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+<g clipPath="url(#clip0_475_45)">
+<path d="M10 5C10 4.46957 10.2107 3.96086 10.5858 3.58579C10.9609 3.21071 11.4696 3 12 3C12.5304 3 13.0391 3.21071 13.4142 3.58579C13.7893 3.96086 14 4.46957 14 5C15.1484 5.54303 16.1274 6.38833 16.8321 7.4453C17.5367 8.50227 17.9404 9.73107 18 11V14C18.0753 14.6217 18.2954 15.2171 18.6428 15.7381C18.9902 16.2592 19.4551 16.6914 20 17H4C4.54494 16.6914 5.00981 16.2592 5.35719 15.7381C5.70457 15.2171 5.92474 14.6217 6 14V11C6.05956 9.73107 6.4633 8.50227 7.16795 7.4453C7.8726 6.38833 8.85159 5.54303 10 5" stroke="#616B77" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M9 17V18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18V17" stroke="#616B77" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
 </g>
 <defs>
 <clipPath id="clip0_475_45">
